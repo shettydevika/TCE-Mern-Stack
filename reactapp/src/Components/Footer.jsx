f18cb718cde6d6aa066e1207dc97cae1a024df22
Devika Shetty <124135758+shettydevika@users.noreply.github.com>
2023-10-21 06:27:17 +0000

    commit

--- reactapp/src/Components/Footer.jsx
+++ reactapp/src/Components/Footer.jsx
@@ -1,17 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+    const cardStyle = {
+      border: '2px solid #ccc',
+      borderRadius: '10px',
+      padding: '20px',
+      textAlign: 'center',
+      boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.1)',
+      backgroundColor: '#f9f9f9'
+    };
+
     return (
-      <div className='text-center nt-4'>
+      <div style={cardStyle}>
         <hr />
-        <h3 className="text-center nt-4">Contact</h3>
-        <b>Mob</b>
-            <p className="text-center nt-4">9876543210</p>
-        <b>Email</b>
-            <p className="text-center nt-4">ds@gmail.com</p>
+        <h3>Contact</h3>
+        <div>
+          <b>Mob</b>
+          <p>9876543210</p>
+        </div>
+        <div>
+          <b>Email</b>
+          <p>ds@gmail.com</p>
+        </div>
       </div>
     );
-  };
-};
+  }
+}
+
 export default Footer;
--- reactapp/src/Components/Header.jsx
+++ reactapp/src/Components/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react' 
-import "./Header.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react';
+import "./Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Header = () => {
   return (
-    <div id='border'>
-      <h1 id="Name" className="text-center nt-4">
-        Devika Shetty
-      </h1><br />
-        <img id="center" src="https://th.bing.com/th/id/OIP.zubvNsaIxKD9T5662YQa2gHaJ4?pid=ImgDet&rs=1.jpeg" alt="Devika Shetty" />
+    <div className='card-container'>
+      <div className='card'>
+        <h1 className="text-center mt-4" >
+          Devika Shetty
+        </h1>
+        <img className="card-image" src="https://th.bing.com/th/id/OIP.zubvNsaIxKD9T5662YQa2gHaJ4?pid=ImgDet&rs=1.jpeg" alt="Devika Shetty" />
+      </div>
     </div>
   );
 };
