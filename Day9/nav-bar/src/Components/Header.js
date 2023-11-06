2458e382170fc4ce83cf3bad4aecc7c8e7a6689f
Devika Shetty <124135758+shettydevika@users.noreply.github.com>
2023-11-06 06:38:37 +0000

    commit

--- Day9/nav-bar/src/Components/Header.js
+++ Day9/nav-bar/src/Components/Header.js
@@ -4,8 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Header(){
     return(
         <div>
-    <h1 id="name" className="text-center mt-">Sinchana PR</h1>
-    <img src="https://tse1.mm.bing.net/th?id=OIP.ihhfDe3fw0xJSEFwYZD5LAHaFj&pid=Api&rs=1&c=1&qlt=95&w=140&h=105" alt="..."/>
+    <h1 id="name" className="text-center mt-">Devika Shetty</h1>
+    <img src="" alt="..."/>
     </div>
     )
 }
--- Day9/nav-bar/src/Pages/Home.js
+++ Day9/nav-bar/src/Pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
 function Home(){
     return(
-        <h1 id="name" className="text-center mt-">Sinchana PR</h1>
+      
+      <div>
+          <h1 id="name" className="text-center mt-4">Devika Shetty</h1>
+
+          <img className='center' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYahhXJfOBFmyIcIMKiEeJcfXICBwUrAjVDg&usqp=CAU' alt='loading..'/>
+      </div>
+        
+
     );
 };
 export default Home;
